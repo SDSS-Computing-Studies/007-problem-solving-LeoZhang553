9b67e16c75048fb0ed0b8735323ea7a4cc7dd43a
LeoZhang553 <leoz553@deltalearns.ca>
2020-10-21 13:11:46 -0700

    s

--- check.py
+++ check.py
@@ -1,17 +1,16 @@
 import pyautogui as p 
 import time as t
 
-hand = p.locateCenterOnScreen('butHand.png')
-baby = p.locateCenterOnScreen('butBaby.png')
-toilet = p.locateCenterOnScreen('butToilet.png')
-cow = p.locateCenterOnScreen('butCow.png')
-farm = p.locateCenterOnScreen('butFarm.png')
-factory = p.locateCenterOnScreen('butFactory.png')
-city = p.locateCenterOnScreen('butCity.png')
-earth = p.locateCenterOnScreen('butEarth.png')
-poop = p.locateCenterOnScreen('butPoop.png')
+hand = p.locateCenterOnScreen('butHand.png', region=(1000,180,900,480))
+baby = p.locateCenterOnScreen('butBaby.png', region=(1000,180,900,480))
+toilet = p.locateCenterOnScreen('butToilet.png', region=(1000,180,900,480))
+cow = p.locateCenterOnScreen('butCow.png', region=(1000,180,900,480))
+farm = p.locateCenterOnScreen('butFarm.png', region=(1000,180,900,480))
+factory = p.locateCenterOnScreen('butFactory.png', region=(1000,180,900,480))
+city = p.locateCenterOnScreen('butCity.png', region=(1000,180,900,480))
+earth = p.locateCenterOnScreen('butEarth.png', region=(1000,180,900,480))
 
-
+x,y = p.locateCenterOnScreen('butPoop.png', confidence=0.8)
 #p.click(hand)
 #t.sleep(0.4)
 #p.click(baby)
@@ -29,5 +28,24 @@ poop = p.locateCenterOnScreen('butPoop.png')
 #p.click(earth)
 #t.sleep(0.4)
 
-if p.pixel(poop) == (186,151,101):
-    print(poop)
+
+hand = p.locateCenterOnScreen('butHand.png')
+baby = p.locateCenterOnScreen('butBaby.png')
+toilet = p.locateCenterOnScreen('butToilet.png')
+cow = p.locateCenterOnScreen('butCow.png')
+farm = p.locateCenterOnScreen('butFarm.png')
+factory = p.locateCenterOnScreen('butFactory.png')
+city = p.locateCenterOnScreen('butCity.png')
+earth = p.locateCenterOnScreen('butEarth.png')
+
+x1,y1=hand
+x1=int(x1)
+y1=int(y1)
+
+print(p.pixel(x1,y1))
+
+x2,y2=baby
+x2=int(x2)
+y2=int(y2)
+
+print(p.pixel(x2,y2))
--- gamerobot.py
+++ gamerobot.py
@@ -1,11 +1,18 @@
 import pyautogui as p 
 import time as t
 
-p.mouseInfo()
 
 # game: https://www.crazygames.com/game/poop-clicker-2
 
 # this is the work for submission
+
+# all locations will be grey buttons
+# locate those locations at the begining of the program
+# in while loop, check the color of the center of the picture(button)
+# use if...else to decide if click it
+
+poop = p.locateCenterOnScreen('butPoop.png')
+tools = p.locateCenterOnScreen('butTools.png')
 hand = p.locateCenterOnScreen('butHand.png')
 baby = p.locateCenterOnScreen('butBaby.png')
 toilet = p.locateCenterOnScreen('butToilet.png')
@@ -14,53 +21,22 @@ farm = p.locateCenterOnScreen('butFarm.png')
 factory = p.locateCenterOnScreen('butFactory.png')
 city = p.locateCenterOnScreen('butCity.png')
 earth = p.locateCenterOnScreen('butEarth.png')
-poop = p.locateCenterOnScreen('butPoop.png')
-tools = p.locateCenterOnScreen('butTools.png')
-
 while True:
-    if p.pixel(poop) == (186,151,101):
-        p.click(poop)  
 
-        if p.pixel(1637,546) == ( 140,314,57 ):
-            p.moveTo()
-            p.moveTo(1637,546)
-            p.click()
-            t.sleep(0.3)
+    p.moveTo(poop)
+    p.click()
+    t.sleep(0.1)
 
-        if p.pixel(1671,430) == (155,221,75):
-            p.moveTo(1671,430)
-            p.click()
-            t.sleep(0.3)
+    x1,y1=hand
+    x1=int(x1)
+    y1=int(y1)
 
-        if p.pixel(1632,336) == (147,217,64):
-            p.moveTo(1632,336)
-            p.click()
-            t.sleep(0.3)
+    print(p.pixel(x1,y1))
 
-        if p.pixel(1645,233) == (147,217,65):
-            p.moveTo(1645,233)
-            p.click()
-            t.sleep(0.3)
+    if p.pixel(x1,y1) == (184, 209, 104):
+        p.moveTo(hand)
+        p.click()
+        t.sleep(0.2)
 
-        if p.pixel(1191,550) == ( 135,211 ,53 ):
-            p.moveTo(1191,550)
-            p.click()
-            t.sleep(0.3)
 
-        if p.pixel( 1176,439) == ( 146,217,63):
-            p.moveTo(1176,439)
-            p.click()   
-            t.sleep(0.3) 
-
-        if p.pixel(1183,337) == ( 146,217 ,63 ):
-            p.moveTo(1183,337)
-            p.click()
-            t.sleep(0.2)
-
-        if p.pixel( 1169,233 ) == ( 147,217 ,64 ):
-            p.moveTo(1169,233)
-            p.click()
-            t.sleep(0.1)
-        
-    else:
-        break
+    
